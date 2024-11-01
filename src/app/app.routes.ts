59e3a89d05942f71404844e11f67b246c98d5d81
rogerr23 <roger17ribeiro09@gmail.com>
2024-11-01 17:46:30 -0300

    First Commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { CadastrarClientesComponent } from './cadastrar-clientes/cadastrar-clientes.component';
+import { ConsultarClientesComponent } from './consultar-clientes/consultar-clientes.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'app/cadastrar-clientes',
+        component: CadastrarClientesComponent
+    },
+    {
+        path: 'app/consultar-clientes',
+        component: ConsultarClientesComponent
+    }
+];
---
+++ src/app/cadastrar-clientes/cadastrar-clientes.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cadastrar-clientes',
+  standalone: true,
+  imports: [],
+  templateUrl: './cadastrar-clientes.component.html',
+  styleUrl: './cadastrar-clientes.component.css'
+})
+export class CadastrarClientesComponent {
+
+}
